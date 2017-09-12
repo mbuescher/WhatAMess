358e5c9035a2f6cf40ebf8f92c1248ce3ecc61d5
mbuescher <mbuescher@hb.edu>
2017-09-12 06:23:55 -0400

    Create WhatAMess.java

---
+++ WhatAMess.java
@@ -0,0 +1,14 @@
+/**
+ * A truly ugly program
+ * Clean it up!
+ * Add appropriate spacing, line breaks, indentation, and all that.
+ * Find the syntax errors so that it compiles.
+ * Find the logic errors so that it works correctly.
+ * Author: Michael Buescher
+ */
+
+import java.util.Scanner; public class WhatAMess {  public static void main(String[] args)
+  { Scanner kboard = new Scanner(System.in); System.out.print("Enter an integer: "); int n = kboard.nextInt(); }
+if (n >= 0); System.out.println (n + " is positive."); if (n <= 0); system.out.println (n + " is negative.");
+if (n == 0); System.out.println (n + " is neither positive nor negative!\nYou found the one magic answer!");
+kboard.close()}}
